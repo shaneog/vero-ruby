9ba2de03290e352b4220d52a5ac140175b92f53b
Shane O'Grady <shane@ogrady.ie>
2015-09-03 18:00:58 -0300

    :art: Clean up code
    According to the Ruby Style Guide

--- lib/vero/ruby.rb
+++ lib/vero/ruby.rb
@@ -1,6 +1,7 @@
-require "vero/ruby/version"
+require 'vero/ruby/version'
 
 module Vero
+  # Vero::Ruby module
   module Ruby
     # Your code goes here...
   end
--- lib/vero/ruby/version.rb
+++ lib/vero/ruby/version.rb
@@ -1,5 +1,6 @@
 module Vero
+  # Version module
   module Ruby
-    VERSION = "0.1.0"
+    VERSION = '0.1.0'
   end
 end
