3e1d699aadc746ef47d451394c8bcd99587b67ee
Shane O'Grady <shane@ogrady.ie>
2015-09-03 19:50:58 -0300

    Add test coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+
+  CodeClimate::TestReporter.start
+end
+
 require 'vero/ruby'
